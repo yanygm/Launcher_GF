6abf2946ece4ec671d561b276c6719088310a3c6
Yany <475016817@msn.cn>
2024-04-18 08:31:18 +0800

    Update Version 3171

--- KartRider.Data/Set_Data/SetGameOption.cs
+++ KartRider.Data/Set_Data/SetGameOption.cs
@@ -9,7 +9,7 @@ namespace Set_Data
 {
 	public static class SetGameOption
 	{
-		public static ushort Version = 3170;
+		public static ushort Version = 3171;
 		public static float Set_BGM = 1f;
 		public static float Set_Sound = 1f;
 		public static byte Main_BGM = 0;
