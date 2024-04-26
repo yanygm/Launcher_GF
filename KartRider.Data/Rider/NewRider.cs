ab156980b56d60d4c42f1a4a08e39a3969e49c10
Yany <475016817@msn.cn>
2024-04-26 22:23:35 +0800

    Update

--- KartRider.Data/Rider/NewRider.cs
+++ KartRider.Data/Rider/NewRider.cs
@@ -191,7 +191,7 @@ namespace RiderData
 						XmlDocument PartsData = new XmlDocument();
 						doc.Load(@"Profile\PartsData.xml");
 						XmlElement AddPartsData = PartsData.SelectSingleNode("//Kart[@id='" + i + "' and @sn='" + sn + "']") as XmlElement;
-						if (AddPartsData == null)
+						if (AddPartsData == null && i > 0)
 						{
 							KartExcData.AddPartsList(i, sn, 63, 0, 0, 0);
 						}
