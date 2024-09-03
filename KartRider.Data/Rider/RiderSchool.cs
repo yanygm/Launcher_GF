c3357ac3b71c0a686b0b011fc4438ca7859b526c
Yany <475016817@msn.cn>
2024-09-03 18:06:10 +0800

    Update RiderSchool.cs

--- KartRider.Data/Rider/RiderSchool.cs
+++ KartRider.Data/Rider/RiderSchool.cs
@@ -12,9 +12,9 @@ namespace RiderData
 			using (OutPacket oPacket = new OutPacket("PrRiderSchoolProPacket"))
 			{
 				oPacket.WriteByte(1);//엠블럼 체크
-				oPacket.WriteByte(31);
+				oPacket.WriteByte(33);
 				oPacket.WriteByte(6);
-				oPacket.WriteByte(32);
+				oPacket.WriteByte(34);
 				oPacket.WriteInt(0);
 				oPacket.WriteInt(0);
 				oPacket.WriteInt(0);
