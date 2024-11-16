99551216065b022c4d9eaf7504cd18d562f382e9
Yany <475016817@msn.cn>
2024-11-16 22:12:37 +0800

    Update ClientSession.cs

--- KartRider.Data/Server/ClientSession.cs
+++ KartRider.Data/Server/ClientSession.cs
@@ -834,6 +834,11 @@ namespace KartRider
 									outPacket.WriteShort(existingList[8]);
 									this.Parent.Client.Send(outPacket);
 								}
+								existingList[2] = 603;
+								existingList[3] = 703;
+								existingList[4] = 903;
+								KartExcData.SaveTuneList(KartExcData.TuneList);
+								TuneSpec.Use_TuneSpec(Kart, KartSN);
 							}
 							else
 							{
